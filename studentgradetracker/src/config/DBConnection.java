708ee7595df55a10b221453b782496c8cf68fe0e
Sampathi Venkateswarlu <sampat.venkateswarlu@gmail.com>
2026-01-12 20:01:23 +0530

    Update DBConnection.java

--- studentgradetracker/src/config/DBConnection.java
+++ studentgradetracker/src/config/DBConnection.java
@@ -6,20 +6,18 @@ import java.sql.SQLException;
 
 public final class DBConnection {
 
-    // ---- DATABASE CONFIGURATION ----
+    
     private static final String DB_URL =
             "jdbc:mysql://localhost:3306/studentgradetracker";
 
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "root";
 
-    // Prevent instantiation
+    
     private DBConnection() {
     }
 
-    /**
-     * Returns a new database connection.
-     */
+   
     public static Connection getConnection() throws SQLException {
 
         try {
